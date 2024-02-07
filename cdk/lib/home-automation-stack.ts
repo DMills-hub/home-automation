b92b11dbd1838d5fb81db81e405f6887f3477742
DMills-hub <maildanmills@gmail.com>
2024-02-07 22:40:09 +0000

    Get rid of domain lambda from stack

--- cdk/lib/home-automation-stack.ts
+++ cdk/lib/home-automation-stack.ts
@@ -2,15 +2,11 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { HomeAutomationFargate } from '../constructs/home-automation/home-automation-fargate'
 import { Config } from '../definitions'
-import { EcsDomainLambda } from '../constructs/management/ecs-domain-lambda'
 
 export class HomeAutomationStack extends cdk.Stack {
-  public readonly ecsDomainLambda: EcsDomainLambda
   public readonly homeAutomationFargate: HomeAutomationFargate
   constructor(scope: Construct, id: string, props: Config) {
     super(scope, id, props)
-
-    this.ecsDomainLambda = new EcsDomainLambda(this, 'EcsDomainLambda')
 
     this.homeAutomationFargate = new HomeAutomationFargate(
       this,
