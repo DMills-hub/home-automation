c85b3d4a2dcd3b1143f560086e0a5394e4ec7e75
DMills-hub <maildanmills@gmail.com>
2024-02-08 00:28:18 +0000

    Finish ecs lambda

--- cdk/constructs/management/ecs-domain-lambda.ts
+++ cdk/constructs/management/ecs-domain-lambda.ts
@@ -3,16 +3,56 @@ import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs'
 import path = require('path')
 import * as events from 'aws-cdk-lib/aws-events'
 import * as eventsTargets from 'aws-cdk-lib/aws-events-targets'
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class EcsDomainLambda extends Construct {
   public fn: lambda.NodejsFunction
+  private role: iam.Role
   constructor(scope: Construct, id: string) {
     super(scope, id)
+
+    const lambdaActionsPolicyDocument = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            'ec2:DescribeNetworkInterfaces',
+            'ecs:DescribeClusters',
+            'ecs:ListTagsForResource',
+            'route53:ChangeResourceRecordSets'
+          ],
+          effect: iam.Effect.ALLOW,
+          resources: ['*']
+        })
+      ]
+    })
+
+    const lambdaLogsPolicyDocument = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            'logs:CreateLogGroup',
+            'logs:CreateLogStream',
+            'logs:PutLogEvents'
+          ],
+          effect: iam.Effect.ALLOW,
+          resources: ['*']
+        })
+      ]
+    })
+
+    this.role = new iam.Role(this, 'EcsDomainLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        EcsDomainLambdaPolicy: lambdaActionsPolicyDocument,
+        EcsDomainLambdaLogsPolicy: lambdaLogsPolicyDocument
+      }
+    })
 
     this.fn = new lambda.NodejsFunction(this, 'EcsDomainLambda', {
       functionName: 'ecs-domain-lambda',
       entry: path.join(__dirname, '../../ecs-domain-lambda/index.ts'),
-      handler: 'handler'
+      handler: 'handler',
+      role: this.role
     })
 
     const rule = new events.Rule(this, 'EcsDomainLambdaEventRule', {
@@ -30,7 +70,5 @@ export class EcsDomainLambda extends Construct {
     const lambdaFnTarget = new eventsTargets.LambdaFunction(this.fn)
 
     rule.addTarget(lambdaFnTarget)
-
-    // this.fn.
   }
 }
